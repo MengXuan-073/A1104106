f756a8d5f70c6e0ddfcb9f03187cbec97b741294
A1104106 <a1104106@mail.nuk.edu.tw>
2025-03-12 17:05:57 +0800

    Add files via upload

---
+++ 0221HW/hw2-2.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>迎新活動報名表單</title>
+</head>
+<body bgcolor="#f5fffa">
+<H3>活動行程表</H3>
+
+<table border="1" width="80%">
+	<tr><td bgcolor="#a9a9a9">時間</td><td bgcolor="#a9a9a9">活動</td><td bgcolor="#a9a9a9">地點</td></tr>
+	<tr><td>18:00~18:30</td><td>簽到與分組</td><td>理學院-1樓 &nbsp; B111階梯教室</td></tr>
+	<tr><td bgcolor="#dcdcdc" colspan="4"><center>中場休息&享用buffet</center></td></tr>
+	<tr><td>18:30~19:30</td><td>活動一 <BR> 系所介紹與小組有獎徵答</td><td>理學院-地下一樓 &nbsp;B101 </td></tr>
+	<tr><td bgcolor="#dcdcdc" colspan="4"><center>中場休息</center></td></tr>
+	<tr><td>19:30~20:30</td><td>活動二 &nbsp; 瘋狂邏輯大風車</td><td>理學院-一樓 &nbsp; 川堂</td></tr>
+	<tr><td bgcolor="#dcdcdc" colspan="4"><center>賦歸</center></td></tr>
+</table>
+<BR>
+
+<hr width="85%" size="2" noshade09:align="center" color="green">
+
+<H2>填寫表單</H2>
+
+<form action="" method=""><BR>
+您的姓名:<input type="text"><BR>
+您的學號:<input type="password"><BR>
+您的電子信箱:<input type="password"><BR>
+您的手機號碼:<input type="password"><BR>
+您的生日:<input type="date"><BR>
+
+
+任何建議或想說的話：<BR>
+<textarea cols="40" row=15></textarea><BR>
+
+<input type="submit"><input type="reset">
+</form>
+
+</body>
+</html>
