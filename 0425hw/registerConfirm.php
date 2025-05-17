43e1371ff69f5e73518059265fdaedaeb715f0c9
A1104106 <a1104106@mail.nuk.edu.tw>
2025-05-17 17:10:27 +0800

    Update registerConfirm.php

--- 0425hw/registerConfirm.php
+++ 0425hw/registerConfirm.php
@@ -55,8 +55,8 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'a1104106@mail.nuk.edu.tw';                     //SMTP username
-    $mail->Password   = 'rwgp ryme srhj mrsy';                               //SMTP password
+    $mail->Username   = '';                     //SMTP username
+    $mail->Password   = '';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     $mail->CharSet = "UTF-8"; // 設定信件內容編碼
